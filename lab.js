3cf4590821583ae5bb8a80452466633b297a94a4
Sutormin Evgeniy <sutormin.evgeniy@yandex.ru>
2016-10-30 20:12:12 +0300

    add obj Eggs drive eggs

---
+++ lab.js
@@ -0,0 +1,31 @@
+// Полифил  для кроссбпаузерности requestAnimationFrame
+(function() {
+  var lastTime = 0;
+  var vendors = ['ms', 'moz', 'webkit', 'o'];
+  for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+    window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+    window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
+                   || window[vendors[x]+'CancelRequestAnimationFrame'];
+  }
+
+  if (!window.requestAnimationFrame)
+    window.requestAnimationFrame = function(callback, element) {
+      var currTime = new Date().getTime();
+      var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+      var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
+        timeToCall);
+      lastTime = currTime + timeToCall;
+      return id;
+    };
+
+  if (!window.cancelAnimationFrame)
+    window.cancelAnimationFrame = function(id) {
+      clearTimeout(id);
+    };
+}());
+
+function randomInteger(min, max) {
+  var rand = min + Math.random() * (max + 1 - min);
+  rand = Math.floor(rand);
+  return rand;
+}
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 (function(){
+  // инициализация canvas
   var canvas = document.getElementById('gameCanvas'),
       ctx = canvas.getContext('2d'),
       gameScreenWidth  = 544, 
@@ -6,6 +7,7 @@
       canvas.width  = gameScreenWidth; 
       canvas.height = gameScreenHeight; 
 
+  // базовый объект фигур
   function Figura(path, width, height, pos){
     this.loadImage(path, width, height, pos);
   }
@@ -68,7 +70,7 @@
     }
   }
 
-
+  // Объект для волка с карзиной
   function Volf(){
     this.volfBody = new Figura("img/wolf-98x152.png", 
                               98, 152, 
@@ -77,11 +79,17 @@
     this.basket = new Figura("img/basket-p-85x72.png", 
                               85, 72, 
                               [[[116, 148],[110, 214]],
-                               [[348, 155],[348, 221]]]);    
+                               [[348, 155],[348, 221]]]);
+    this.curPos = [0, 0]
   }
-  Volf.prototype.drawImage = function (pos0, pos1) {
-    this.volfBody.drawImage(pos0, 0);
-    this.basket.drawImage(pos0, pos1);
+  Volf.prototype.setPos = function (pos) {
+    for(var i = 0; i < pos.length; i++){
+      this.curPos[i] = pos[i];
+    }
+  }
+  Volf.prototype.drawImage = function () {
+    this.volfBody.drawImage(this.curPos[0], 0);
+    this.basket.drawImage(this.curPos[0], this.curPos[1]);
   }
   Volf.prototype.drawImageAll = function () {
     this.volfBody.drawImage(0, 0);
@@ -92,35 +100,124 @@
     this.basket.drawImage(1, 1);
   }
 
-  var volf = new Volf();
-  volf.drawImageAll();
+  // Объект для волка с падающих яиц
+  function Egg(egg, pos){
+    this.egg = egg;
+    this.curPos = [0, 0, 0];
+    this.curCoord = [0, 0, 0];
 
-  function Egg(){
+    this.curPos[1] = pos % 2;
+    this.curPos[0] = (pos - this.curPos[1]) / 2;
+    this.curCoord = [0, 0, 0];
+    this.calculatePos();
+  }
+  Egg.prototype.calculatePos = function () {
+    var coordPos = [[32, 107, 0.6],
+                    [49, 117, 1.2],
+                    [65, 128, 3],
+                    [79, 137, 4.5],
+                    [94, 145, 5.9]];
+    var x = this.curPos[0], 
+        y = this.curPos[1];
+
+    this.curCoord[0] = ((gameScreenWidth - 22)*x) + ((1-2*x) * coordPos[this.curPos[2]][0]);
+    this.curCoord[1] = (y*73) + coordPos[this.curPos[2]][1];
+    this.curCoord[2] = (1-2*x) * coordPos[this.curPos[2]][2];
+  }
+  Egg.prototype.drawImage = function () {
+    this.egg.draw(0, 0, this.curCoord[0], this.curCoord[1], this.curCoord[2]);
+  }
+
+  // Объект для волка с падающих яиц
+  function Eggs(){
     this.egg = new Figura("img/egg-19x19.png", 
                               19, 19, 
                               [[[166, 152]],
                                [[287, 152]]]);
+    this.eggs = [];
+    this.numb = 0;
   }
-  Egg.prototype.drawImage = function (pos0, pos1) {
-    this.egg.drawImage(pos0, pos1);
+  Eggs.prototype.addEgg = function () {
+    // получение случайной позиции яца кроме предыдущей
+    var numbArr = [0, 1, 2, 3],
+        l = 3;
+    if(this.eggs.length > 0){
+      l = 2;
+      numbArr.splice(this.numb, 1);
+    }
+    this.numb = numbArr[randomInteger(0, l)];
+    var newEgg = new Egg(this.egg, this.numb);
+    
+    this.eggs.push(newEgg);
   }
-  Egg.prototype.drawImageAll = function () {
+  Eggs.prototype.deleteEgg = function (i) {
+    this.eggs.splice(i, 1);
+  }
+  Eggs.prototype.increnent = function () {
+    for(var i = (this.eggs.length - 1); i >= 0; i--){
+      if(this.eggs[i].curPos[2] < 4){
+        this.eggs[i].curPos[2]++;
+        this.eggs[i].calculatePos();
+      } else {
+        this.deleteEgg(i);
+      }
+    }
+    this.addEgg(i);
+  }
+  Eggs.prototype.drawImage = function () {
+    for(var i = 0; i < this.eggs.length; i++){
+      this.eggs[i].drawImage();
+    }
+  }
+  Eggs.prototype.drawImageAll = function () {
     this.egg.draw(0, 0, 32, 107, 0.6);
     this.egg.draw(0, 0, 49, 117, 1.2);
     this.egg.draw(0, 0, 65, 128, 3);
     this.egg.draw(0, 0, 79, 137, 4.5);
     this.egg.draw(0, 0, 94, 145, 5.9);
 
+    this.egg.draw(0, 0, 32, 107 + 73, 0.6);
+    this.egg.draw(0, 0, 49, 117 + 73, 1.2);
+    this.egg.draw(0, 0, 65, 128 + 73, 3);
+    this.egg.draw(0, 0, 79, 137 + 73, 4.5);
+    this.egg.draw(0, 0, 94, 145 + 73, 5.9);
+
     this.egg.draw(0, 0, gameScreenWidth - 22 - 32, 107, -0.6);
     this.egg.draw(0, 0, gameScreenWidth - 22 - 49, 117, -1.2);
     this.egg.draw(0, 0, gameScreenWidth - 22 - 65, 128, -3);
     this.egg.draw(0, 0, gameScreenWidth - 22 - 79, 137, -4.5);
     this.egg.draw(0, 0, gameScreenWidth - 22 - 94, 145, -5.9);
+
+    this.egg.draw(0, 0, gameScreenWidth - 22 - 32, 107 + 73, -0.6);
+    this.egg.draw(0, 0, gameScreenWidth - 22 - 49, 117 + 73, -1.2);
+    this.egg.draw(0, 0, gameScreenWidth - 22 - 65, 128 + 73, -3);
+    this.egg.draw(0, 0, gameScreenWidth - 22 - 79, 137 + 73, -4.5);
+    this.egg.draw(0, 0, gameScreenWidth - 22 - 94, 145 + 73, -5.9);
   }
 
-  var egg = new Egg();
-  egg.drawImageAll();
+  var volf = new Volf();
+  var eggs = new Eggs();
+  volf.drawImageAll();
+  eggs.drawImageAll();
 
+  var speed = 50,
+        step = 0;
+  stepAnimation();
+
+  function stepAnimation() {
+    requestAnimationFrame(stepAnimation);
+
+    //очистка экрана
+    ctx.clearRect(0, 0, gameScreenWidth, gameScreenHeight);
+    // рисование фигур
+    volf.drawImage();
+    eggs.drawImage();
+    
+    if(++step >= speed){
+      step = 0;
+      eggs.increnent();
+    }
+  }
   // обработчик кнопок управления
   var bottonsControl = document.querySelectorAll('.gameControl');
   for(var i=0; i<bottonsControl.length; i++){
@@ -129,11 +226,8 @@
       var bottonControl = event.target;
       var pos = bottonControl.getAttribute('data-control').split('-');
 
-      //очистка экрана
-      ctx.clearRect(0, 0, gameScreenWidth, gameScreenHeight);
-
-      // прорисовка фигур
-      volf.drawImage(pos[0], pos[1]);
+      // смена позиции волка
+      volf.setPos(pos);
     });
   }
 
